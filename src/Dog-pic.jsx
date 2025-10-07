a63b28896669dd7cb721c2ccb9dd30df1af870d3
createlsworklappy <justinecastillo7@gmail.com>
2025-10-07 14:57:52 +0900

    func: create Greeting function to serve a greeting quote
    create DogPic function to serve dawg

---
+++ src/Dog-pic.jsx
@@ -0,0 +1,5 @@
+function DogPic() {
+  return <img id="dawg" src="./public/9140062_02.jpg"></img>
+}
+
+export {DogPic};
---
+++ src/Greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting() {
+  return <h1>&quot;I got that DAWG in me.&quot;</h1>;
+}
+
+export default Greeting;
