24e0fbf098237643e6d063116a6b71c9a2d6c3f4
createlsworklappy <justinecastillo7@gmail.com>
2025-10-07 15:15:50 +0900

    func: write test jsx markup with appropriate syntax

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import Greeting from './Greeting.jsx'
 import { DogPic } from './Dog-pic.jsx'
+import { TestTitle } from './new-component-syntax.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Greeting />
     <DogPic />
+    <TestTitle />
   </StrictMode>,
 )
